f60f98862bbcaa864d1aaf71ed2a6f243f4935b2
DianaKiriienko <diana.kirienko.homiak@gmail.com>
2024-04-28 22:08:53 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,15 +7,15 @@ import css from './ContactForm.module.css';
 
 const ContactsForm = () => {
   const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [number, setPhoneNumber] = useState('');
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name.trim() === '' || phoneNumber.trim() === '') {
+    if (name.trim() === '' || number.trim() === '') {
       toast.error("Name and phone number are required."); 
     } else {
-      dispatch(addContact({ name, phoneNumber }));
+      dispatch(addContact({ name, number }));
       setName('');
       setPhoneNumber('');
     }
@@ -26,7 +26,7 @@ const ContactsForm = () => {
       <ToastContainer /> 
       <form onSubmit={handleSubmit} className={css.form}>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" className={css.input} />
-        <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="Phone Number" className={css.input} />
+        <input type="text" value={number} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="Phone Number" className={css.input} />
         <button type="submit" className={css.btn}>Add Contact</button>
       </form>
     </div>
